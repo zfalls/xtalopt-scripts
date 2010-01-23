2ad719db23e78bdb5f12c4acd5d00c1c9e36876d
David Lonie <loniedavid@gmail.com>
2010-01-23 13:33:23 -0500

    Add a halflife calculation, cleanup and comment code.

--- work.py
+++ work.py
@@ -2,6 +2,14 @@
 from dataAnalysis import *
 import sys
 
+#
+# Average energy from a random analysis:
+#
+ave_energy = -622.52807
+
+#
+# Read data in from files on command line
+#
 xs = []
 ys = []
 fs = []
@@ -16,6 +24,7 @@ for file in sys.argv:
     x = []
     y = []
 
+    # Remove bad values, e.g. errored xtals
     for i in range(len(n)):
         if abs(m[i]) > 10:
             x.append(n[i])
@@ -32,11 +41,15 @@ for file in sys.argv:
     xs.append(x)
     ys.append(y)
     fs.append(f)
+    # Create a summary plot for each data set
     scatter(x,y)
     plot(x,f)
     savefig("%s-summary.png"%file)
     cla()
 
+#
+# Prepare the data for a Hartke plot
+#
 minfs = []
 maxfs = []
 avefs = []
@@ -62,48 +75,9 @@ for point in x:
     maxfs.append(maxf)
     avefs.append(avef)
 
-plot(x,minfs)
-plot(x,maxfs)
-plot(x,avefs)
-
-minf = min(minfs)
-
-def fitFunction2(c,x,y,get_y=False):
-#    y_model = -minf*(e**(-c[0]*x+c[1])) + minf
-    y_model = (e**(c[0]*x**c[1] + c[2]*x)) + minf
-    if get_y == True: return y_model
-    error = y-y_model
-    return error
-
-plotFitData(x,avefs,"Structure number", "Enthalpy (eV)", "Hartke Plot", "custom", plotData=False, color='g', fitFormat=':', 
-            guess=array([-1,1,-1]), func=fitFunction2, fitLabel="regEqu", customFitLabel="$e^{(%%s)x^{%%s}+(%%s)x}+%s$"%(minf))
-
-def fitFunction(c,x,y,get_y=False):
-#    y_model = -minf*(e**(-c[0]*x+c[1])) + minf
-    y_model = -minf*(e**(c[0]*x**c[1] + c[2]*x)) + minf
-    if get_y == True: return y_model
-    error = y-y_model
-    return error
-
-plotFitData(x,avefs,"Structure number", "Enthalpy (eV)", "Hartke Plot", "custom", plotData=False, color='r', fitFormat=':', 
-            guess=array([-1,1,-1]), func=fitFunction, fitLabel="regEqu", customFitLabel="$%se^{(%%s)x^{%%s}+(%%s)x}+%s$"%(-minf,minf))
-
-def fitFunction1(c,x,y,get_y=False):
-#    y_model = -minf*(e**(-c[0]*x+c[1])) + minf
-    y_model = -minf*(e**(c[0]*x**c[1])) + minf
-    if get_y == True: return y_model
-    error = y-y_model
-    return error
-
-plotFitData(x,avefs, "Structure number", "Enthalpy (eV)", "Hartke Plot", "custom", plotData=False, color='b', fitFormat=':', 
-            guess=array([-1,1]), func=fitFunction1, fitLabel="regEqu", customFitLabel="$%se^{(%%s)x^{%%s}}+%s$"%(-minf,minf))
-
-prop = matplotlib.font_manager.FontProperties(size=15)
-legend(loc=1, prop = prop)
-savefig("hartke.png")
-
-cla()
-
+#
+# Create a plot of the percent of runs completed by structure
+#
 percents = []
 for point in range(minlen):
     done = 0
@@ -117,3 +91,54 @@ for point in range(minlen):
 plotFitData(x, percents, "Structure number", "Percent of runs with lowest energy structure", "Percent complete by structure", 
             plotData=False, regType="connect")
 savefig("percents.png")
+cla()
+
+#
+# Generate the Hartke plot
+#
+plot(x,maxfs, color='b', label="Worst-best structure")
+plot(x,avefs, color='r', label="Average best structure")
+plot(x,minfs, color='g', label="Best-best structure")
+
+# Fit average f function
+E_min = min(minfs)
+E_0 = ave_energy-E_min
+def bestFitFunction(c,x,y,get_y=False):
+    y_model = E_0*(e**(c[0]*x**c[1] + c[2]*x)) + E_min
+    if get_y == True: return y_model
+    error = y-y_model
+    return error
+
+reg,err,rsq = plotFitData(x,avefs, "Structure number", "Enthalpy (eV)", "Hartke Plot", 
+                          "custom", plotData=False, color='r', fitFormat=':', 
+                          guess=array([-1,1,1]), func=bestFitFunction, fitLabel="regEqu", 
+                          customFitLabel="$%se^{(%%s)x^{%%s}+(%%s)x}+%s$"%(E_0,E_min))
+
+# Solve for halflife with Newton-Raphson:
+tol = 1e-10
+guess = 100
+diff = 1e-5
+
+const = -log(1/2.0)
+def bestHalfLife(x):
+    return reg[0]*x**reg[1] + reg[2]*x + const
+x = guess
+val = bestHalfLife(x)
+print E_0, E_min, const, reg, x, val
+while (abs(val) > tol):
+    dx = (bestHalfLife(x+diff) - val)/diff
+    x = x - val/dx
+    val = bestHalfLife(x)
+    print "Halflife: %.5f, value: %.6f, dx: %.6f"%(x,val,dx)
+
+y = bestFitFunction(reg,x,0,True)
+print "At structure %.5f, the energy should be at %.5f (compare to %.5f)"%(x,
+                                                                           y,
+                                                                           E_0/2.0+E_min)
+
+plot(x,y,'x', color='k', label="Halflife of average (x=%.5f)"%x)
+
+prop = matplotlib.font_manager.FontProperties(size=10)
+legend(loc=1, prop = prop)
+savefig("hartke.png", dpi=150)
+cla()
